374f923491da73e6e5dca52c25f5df8485f756d2
Veranika Zakirava <veranika.zakirava@gmail.com>
2020-10-22 04:04:22 +0300

    Практика часть 2. Добавлен цикл для вопросов, добавлено сообщение в зависимости от количества просмотренных фильмов (upd script.js filename)

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-Проверить, чтобы все работало без ошибок в консоли */
+4) Потренироваться и переписать цикл еще двумя способами*/
 
 'use strict';
 
-// 1)
-
 let numberOfFIlms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-// 2)
 
 const personalMovieDB = {
     count: numberOfFIlms,
@@ -37,14 +24,47 @@ const personalMovieDB = {
     privat: false
 };
 
-// 3) 
+for (let i = 0; i < 2; i++) {
+    // Отдельный if для каждого поля выбран, чтобы при неправильно введенном ответе
+    // начать вводить запись заново сразу, не отвечая следующий вопрос
 
-let currentFilm = prompt('Один из последних просмотренных фильмов?', '');
-let currentFilmRating = +prompt('На сколько оцените его?', '');
+    let currentFilm = prompt('Один из последних просмотренных фильмов?', '');
 
-personalMovieDB.movies[currentFilm] = currentFilmRating;
+    if (currentFilm == '' || currentFilm == null || currentFilm.length > 50) {
+        i--;
+        continue;
+    }
 
-currentFilm = prompt('Один из последних просмотренных фильмов?', '');
-currentFilmRating = +prompt('На сколько оцените его?', '');
+    let currentFilmRating = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[currentFilm] = currentFilmRating;
+    if (currentFilmRating == '' || currentFilmRating == null) {
+        i--;
+        continue;
+    }
+
+    personalMovieDB.movies[currentFilm] = currentFilmRating;
+
+    // Более короткий вариант
+
+    /*
+    let currentFilm = prompt('Один из последних просмотренных фильмов?', ''),
+        currentFilmRating = +prompt('На сколько оцените его?', '');
+
+    if (currentFilm != '' && currentFilm != null && currentFilm.length > 50 &&
+        currentFilmRating != '' || currentFilmRating != null) {
+        
+        personalMovieDB.movies[currentFilm] = currentFilmRating;
+    } else {
+        i--;
+    }*/
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
