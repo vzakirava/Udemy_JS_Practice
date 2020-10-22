b08fbbfd1ab5c1c571ff4e6849df1201e3b0d42b
Veranika Zakirava <veranika.zakirava@gmail.com>
2020-10-22 03:39:47 +0300

    Revert "Практика часть 2. Добавлен цикл для вопросов, добавлено сообщение в зависимости от количества просмотренных фильмов"
    This reverts commit 9e794ccd398d77d257044e0be8b54fe159f6a784.
    Renaming of the 'script12.js' file didn't go as planned.

---
+++ script12.js
@@ -0,0 +1,50 @@
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict';
+
+// 1)
+
+let numberOfFIlms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// 2)
+
+const personalMovieDB = {
+    count: numberOfFIlms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+// 3) 
+
+let currentFilm = prompt('Один из последних просмотренных фильмов?', '');
+let currentFilmRating = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[currentFilm] = currentFilmRating;
+
+currentFilm = prompt('Один из последних просмотренных фильмов?', '');
+currentFilmRating = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[currentFilm] = currentFilmRating;
