26172b76b9e13f2cf4551524f96ea986cff29fb8
Veranika Zakirava <veranika.zakirava@gmail.com>
2020-11-26 17:30:24 +0300

    Новый проект: сайт доставки
    Изменения script.js:
        Реализовано переключение вкладок в меню выбора стиля питания

---
+++ Food Site/js/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Tabs
+    
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabContainer = document.querySelector('.tabcontainer');
+
+    hideTabContent();
+    showTabContent(0);
+
+    function hideTabContent() {
+        tabsContent.forEach(tab => {
+            tab.classList.add('hide');
+            tab.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(tab => {
+            tab.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    tabContainer.addEventListener('click', (event) => {
+        const target = event.target;
+        
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((tab, i) => {
+                if (target == tab) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
